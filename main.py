02123a80dcfaae0a22c353917fc9005e83cd1e51
Raymond Smith <raymondsmith@Raymonds-MacBook-Pro-2.local>
2023-03-13 18:00:13 -0700

    Update Predict Rates

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def create_pr(countries: list[str]) -> None:
         pr.rf_regression(pred[country])
 
 def main():
-    countries = ['Canada', 'korea']
+    countries = ['Canada', 'australia']
     create_pr(countries)
 
 if __name__ == "__main__":
